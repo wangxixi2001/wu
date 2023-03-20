072f31f9709423288ae99b7baedd67bb7f2d1d87
wangxixi2001 <128358219+wangxixi2001@users.noreply.github.com>
2023-03-20 16:56:28 +0800

    Update edu.go

--- chaincode/edu.go
+++ chaincode/edu.go
@@ -12,25 +12,13 @@ import (
 
 type Education struct {
 	ObjectType	string	`json:"docType"`
-	Name	string	`json:"Name"`		// 姓名
-	Gender	string	`json:"Gender"`		// 性别
-	Nation	string	`json:"Nation"`		// 民族
-	EntityID	string	`json:"EntityID"`		// 身份证号
-	Place	string	`json:"Place"`		// 籍贯
-	BirthDay	string	`json:"BirthDay"`		// 出生日期
-
-	EnrollDate	string	`json:"EnrollDate"`		// 入学日期
-	GraduationDate	string	`json:"GraduationDate"`	// 毕（结）业日期
-	SchoolName	string	`json:"SchoolName"`	// 学校名称
-	Major	string	`json:"Major"`	// 专业
-	QuaType	string	`json:"QuaType"`	// 学历类别
-	Length	string	`json:"Length"`	// 学制
-	Mode	string	`json:"Mode"`	// 学习形式
-	Level	string	`json:"Level"`	// 层次
-	Graduation	string	`json:"Graduation"`	// 毕（结）业
+	AssetName	string	`json:"Name"`		// 姓名
+	OwnerID	string	`json:"Gender"`		// 性别
+	State	string	`json:"Nation"`		// 民族
+	Version	string	`json:"Place"`		// 籍贯
 	CertNo	string	`json:"CertNo"`	// 证书编号
-
-	Photo	string	`json:"Photo"`	// 照片
+        Ciphertext	string	`json:"Graduation"`	// 毕（结）业
+	Note	string	`json:"Photo"`	// 照片
 
 	Historys	[]HistoryItem	// 当前edu的历史记录
 }
@@ -202,10 +190,10 @@ func (t *EducationChaincode) queryEduByCertNoAndName(stub shim.ChaincodeStubInte
 	// 查询数据
 	result, err := getEduByQueryString(stub, queryString)
 	if err != nil {
-		return shim.Error("根据证书编号及姓名查询信息时发生错误")
+		return shim.Error("根据证书编号及资产名称查询信息时发生错误")
 	}
 	if result == nil {
-		return shim.Error("根据指定的证书编号及姓名没有查询到相关的信息")
+		return shim.Error("根据指定的证书编号及资产名称没有查询到相关的信息")
 	}
 	return shim.Success(result)
 }
