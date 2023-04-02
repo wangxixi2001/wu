c94c123e51bf7d5343303322657310a4d51f103f
wangxixi2001 <128358219+wangxixi2001@users.noreply.github.com>
2023-04-02 12:00:40 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,9 +12,10 @@ import (
 )
 
 const (
-	cc_name = "simplecc"
+	cc_name    = "simplecc"
 	cc_version = "1.0.0"
 )
+
 func main() {
 	// init orgs information
 	orgs := []*sdkInit.OrgInfo{
@@ -26,7 +27,6 @@ func main() {
 			OrgPeerNum:    1,
 			OrgAnchorFile: os.Getenv("GOPATH") + "/src/wu/fixtures/channel-artifacts/Org1MSPanchors.tx",
 		},
-
 	}
 
 	// init sdk env info
@@ -38,7 +38,7 @@ func main() {
 		OrdererOrgName:   "OrdererOrg",
 		OrdererEndpoint:  "orderer.example.com",
 		ChaincodeID:      cc_name,
-		ChaincodePath:    os.Getenv("GOPATH")+"/src/wu/chaincode/",
+		ChaincodePath:    os.Getenv("GOPATH") + "/src/wu/chaincode/",
 		ChaincodeVersion: cc_version,
 	}
 
@@ -64,39 +64,38 @@ func main() {
 	// invoke chaincode set status
 	fmt.Println(">> 通过链码外部服务设置链码状态......")
 
-	edu := service.Education{
-		AssetName: "Labor Contract01",
-		OwnerID: "101",
-		State: "valid",
-		Version:"1.0",
-		CertNo: "111",
+	cert := service.Certificate{
+		AssetName:  "Labor Contract01",
+		OwnerID:    "101",
+		Key:        "abc&1*~#^2^#s0^=)^^7%b34",
+		State:      "valid",
+		Version:    "1.0",
+		CertNo:     "111",
 		Ciphertext: "uQyiNNQr5tZMuNpZqooMkg==",
-		Note: ".......",
-		
+		Note:       ".......",
 	}
 
 	serviceSetup, err := service.InitService(info.ChaincodeID, info.ChannelID, info.Orgs[0], sdk)
-	if err!=nil{
+	if err != nil {
 		fmt.Println()
 		os.Exit(-1)
 	}
-	msg, err := serviceSetup.SaveEdu(edu)
-	if err != nil {
-		fmt.Println(err.Error())
-	}else {
-		fmt.Println("信息发布成功, 交易编号为: " + msg)
-	}
-
-	result, err := serviceSetup.FindEduInfoByEntityID("101")
+	msg, err := serviceSetup.SaveCert(cert)
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
-		var edu service.Education
-		json.Unmarshal(result, &edu)
-		fmt.Println("根据身份证号码查询信息成功：")
-		fmt.Println(edu)
+		fmt.Println("信息发布成功, 交易编号为: " + msg)
 	}
 
+	result, err := serviceSetup.FindCertInfoByOwnerID("101")
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		var edu service.Certificate
+		json.Unmarshal(result, &edu)
+		fmt.Println("OwnerID query info success：")
+		fmt.Println(edu)
+	}
 
 	app := controller.Application{
 		Setup: serviceSetup,
