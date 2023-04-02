ba395da49c59ac6c19abc62dac499a68ac30fe15
wangxixi2001 <128358219+wangxixi2001@users.noreply.github.com>
2023-04-02 12:12:55 +0800

    Update webServer.go

--- web/webServer.go
+++ web/webServer.go
@@ -12,9 +12,9 @@ import (
 
 
 // 启动Web服务并指定路由信息
-func WebStart(app controller.Application)  {
+func WebStart(app controller.Application) {
 
-	fs:= http.FileServer(http.Dir("web/static"))
+	fs := http.FileServer(http.Dir("web/static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	// 指定路由信息(匹配请求)
@@ -25,18 +25,17 @@ func WebStart(app controller.Application)  {
 	http.HandleFunc("/index", app.Index)
 	http.HandleFunc("/help", app.Help)
 
-	http.HandleFunc("/addEduInfo", app.AddEduShow)	// 显示添加信息页面
-	http.HandleFunc("/addEdu", app.AddEdu)	// 提交信息请求
+	http.HandleFunc("/addEduInfo", app.AddCertShow) // 显示添加信息页面
+	http.HandleFunc("/addEdu", app.AddCert)         // 提交信息请求
 
-	http.HandleFunc("/queryPage", app.QueryPage)	// 转至根据证书编号与姓名查询信息页面
-	http.HandleFunc("/query", app.FindCertByNoAndName)	// 根据证书编号与姓名查询信息
+	http.HandleFunc("/queryPage", app.QueryPage)       // 转至根据证书编号与姓名查询信息页面
+	http.HandleFunc("/query", app.FindCertByNoAndName) // 根据证书编号与姓名查询信息
 
-	http.HandleFunc("/queryPage2", app.QueryPage2)	// 转至根据身份证号码查询信息页面
-	http.HandleFunc("/query2", app.FindByID)	// 根据身份证号码查询信息
+	http.HandleFunc("/queryPage2", app.QueryPage2) // 转至根据身份证号码查询信息页面
+	http.HandleFunc("/query2", app.FindByID)       // 根据身份证号码查询信息
 
-
-	http.HandleFunc("/modifyPage", app.ModifyShow)	// 修改信息页面
-	http.HandleFunc("/modify", app.Modify)	//  修改信息
+	http.HandleFunc("/modifyPage", app.ModifyShow) // 修改信息页面
+	http.HandleFunc("/modify", app.Modify)         //  修改信息
 
 	http.HandleFunc("/upload", app.UploadFile)
 
@@ -47,6 +46,3 @@ func WebStart(app controller.Application)  {
 	}
 
 }
-
-
-
